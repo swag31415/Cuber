200a89bc00cbe59528c8a7801643a769896fa7b5
swag31415 <kidswagatam18@gmail.com>
2019-05-03 17:46:28 -0400

    Added a Color object to hold colors

---
+++ src/main/java/Cuber/Color.java
@@ -0,0 +1,38 @@
+package Cuber;
+
+public class Color {
+
+    public enum Colors {
+        White('W'), Blue('B'), Green('G'), Orange('O'), Red('R'), Yellow('Y');
+
+        private char sym;
+
+        private Colors(char sym) {
+            this.sym = sym;
+        }
+
+        @Override
+        public String toString() {
+            return sym + "";
+        }
+    }
+
+    private Colors colorVal;
+
+    public Color(Colors colorVal) {
+        this.colorVal = colorVal;
+    }
+
+    public Colors getColor() {
+        return this.colorVal;
+    }
+
+    public void setColor(Colors colorVal) {
+        this.colorVal = colorVal;
+    }
+
+    @Override
+    public String toString() {
+        return getColor().toString();
+    }
+}
