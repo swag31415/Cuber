9f89607c56d9e58767f2785e935ddfbee2a10830
swag31415 <kidswagatam18@gmail.com>
2019-05-05 12:20:24 -0400

    Added imports, fields, gets and a constructor to Cuber

--- src/main/java/Cuber/Cuber.java
+++ src/main/java/Cuber/Cuber.java
@@ -1,5 +1,47 @@
 package Cuber;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+import Cuber.Cube.Color;
+import Cuber.Cube.Cube;
+import Cuber.Cube.Moves;
+
 public class Cuber {
-    
+
+    List<Moves[]> algList;
+    int algLength;
+    int cubeDim;
+    Moves[] bestAlg;
+    int bestAlgErr;
+
+    public Cuber(int cubeDim, int algLength) {
+        this.cubeDim = cubeDim;
+        this.algLength = algLength;
+        this.bestAlgErr = 60;
+        this.algList = new LinkedList<Moves[]>();
+    }
+
+    public List<Moves[]> getAlgList() {
+        return this.algList;
+    }
+
+    public int getAlgLength() {
+        return this.algLength;
+    }
+
+    public int getCubeDim() {
+        return this.cubeDim;
+    }
+
+    public int getBestAlgErr() {
+        return this.bestAlgErr;
+    }
+
+    public Moves[] getBestAlg() {
+        return this.bestAlg;
+    }
+
+
 }
