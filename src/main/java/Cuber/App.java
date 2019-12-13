a043d875a496f03bc00a80ceefd54de7b130edef
swag31415 <kidswagatam18@gmail.com>
2019-12-13 02:40:50 -0700

    Added needed packages and structuring for a JavaFX project

---
+++ src/main/java/Cuber/App.java
@@ -0,0 +1,67 @@
+package Cuber;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    private static Stage pStage;
+    private static App instance;
+
+    public enum fxmlFiles {
+        Splash("Splash.fxml");
+
+        private String file;
+
+        private fxmlFiles(String file) {
+            this.file = file;
+        }
+
+        public String get() {
+            return this.file;
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        App.pStage = primaryStage;
+
+        primaryStage.setResizable(false);
+
+        App.loadScene(fxmlFiles.Splash, App.pStage);
+        primaryStage.show();
+    }
+
+    @Override
+    public void init() {
+        App.instance = this;
+    }
+
+    public static <T extends Controller> T loadScene(fxmlFiles file, Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(App.getResource(file.get()));
+        Pane pane = loader.<Pane>load();
+        stage.setScene(new Scene(pane));
+        return loader.getController();
+    }
+
+    public static <T extends Pane> T getFxml(fxmlFiles file) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(App.getResource(file.get()));
+        return loader.<T>load();
+    }
+
+    public static URL getResource(String file) {
+        return App.instance.getClass().getClassLoader().getResource(file);
+    }
+}
--- src/main/java/Cuber/Cuber.java
+++
@@ -1,5 +0,0 @@
-package Cuber;
-
-public class Cuber {
-    
-}
