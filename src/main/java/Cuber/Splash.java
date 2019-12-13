4df302a61f305cabd629e5acd7c6a49caf0643b4
swag31415 <kidswagatam18@gmail.com>
2019-12-13 02:53:45 -0700

    Added the GUI

---
+++ src/main/java/Cuber/Splash.java
@@ -0,0 +1,23 @@
+package Cuber;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+
+public class Splash extends Controller {
+
+    @FXML private Slider Iterations;
+    @FXML private Slider CubeDim;
+    @FXML private Slider AlgLength;
+    @FXML private Label TOut;
+
+    @FXML private void Generate(ActionEvent event) {
+
+    }
+
+    @Override
+    protected void init() {
+
+    }
+}
