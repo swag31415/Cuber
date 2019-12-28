7d8c0b5b5e48028b859344d743afb24faefddaa8
swag31415 <kidswagatam18@gmail.com>
2019-12-28 17:03:04 -0500

    added cube turns to the native, it is now a functioning cube!

--- src/main/java/Cuber/Cuber.java
+++ src/main/java/Cuber/Cuber.java
@@ -8,12 +8,19 @@ public class Cuber {
 
     public interface Cube {
         void init(int dim);
+        void e_turn(int ind);
+        void m_turn(int ind);
+        void s_turn(int ind);
         void disp();
     }
 
     public Cuber() {
         Cube cube = LibraryLoader.create(Cube.class).load(cube_dll);
         cube.init(3);
+        cube.disp();
+        cube.e_turn(0);
+        cube.m_turn(1);
+        cube.s_turn(0);
         cube.disp();
     }
 }
